bb949a6e5b35ac29f4c58e04e430ae01fed9768e
DamienLAVAINE <damien.lavaine@live.com>
2026-08-19 00:46:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const tracks = [
   { name: "Medley", file: "audio/MEDLEY - Enter rock to hell.mp3", duration: "6:14" }, 
   { name: "Song2(+0.5)/BLUR", file: "audio/BLUR - Song 2 (+ 0,5 ton).mp3", duration: "2:02" }, 
   { name: "Cause toujours/ECHOES", file: "audio/ZOOM0236 - Cause toujours 26.05 remixé 29.05-3.mp3", duration: "4:29" },
-  { name: "The Pretender(avec presentation Echoes)/FOO FIGHTERS", file: "audio/Foo Fighters - The Pretender (version Echoes).mp3", duration: "6:18" },
+  { name: "The Pretender(avec presentation Echoes)/FOO FIGHTERS", file: "audio/Foo Fighters - The Pretender (version Echoes - sans presentation).mp3", duration: "4:33" },
   { name: "Zombie/THE CRANBERRIES", file: "audio/THE CRANBERRIES - Zombie.mp3", duration: "5:07" },
   { name: "7 Nation army/THE WHITE STRIPES", file: "audio/THE WHITE STRIPES - 7 Nation army (version Echoes).MP3", duration: "3:47" },
   { name: "Nothing else matters(-1ton)/METALLICA", file: "audio/METALLICA - Nothing Else Matters (-1 ton).mp3", duration: "6:28" }
